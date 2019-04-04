03dc9226ec679146ab0319888c7b24c95cfcc7d0
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-04 09:00:56 -0700

    Add files via upload

---
+++ operators/operators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 2;
+            var c = 4;
+            Console.WriteLine(!(c > b || c == a));
+        }
+    }
+}
